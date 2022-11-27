966ab2b7576af21829ab385e58004738981897e7
Extauren <theodoregozard@protonmail.com>
2022-11-27 14:17:10 +0900

    add header

---
+++ Frontend/src/pages/Header.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import BeerLogo from './beerLogo.png';
+
+
+export default function Header() {
+    return (
+        <div className="w-full bg-indigo-400">
+            <div className="flex inline-block">
+                <img src={BeerLogo} className="h-16"/>
+                <h1 className="font-bold mt-4 ml-2 text-xl">Beer Recommendation System</h1>
+            </div>
+        </div>
+    )
+}
--- Frontend/src/pages/Questionnaire.tsx
+++ Frontend/src/pages/Questionnaire.tsx
@@ -6,6 +6,7 @@ import FormControlLabel from '@mui/material/FormControlLabel';
 import Radio from '@mui/material/Radio';
 import RadioGroup from '@mui/material/RadioGroup';
 import Grid from '@mui/material/Grid';
+import Header from './Header';
 
 export default function Questionnaire(props: any) {
     const [questions, setQuestion] = React.useState<any>(["What type of beer do you want ?", "How much alcool do you want ?", "Do you want organic beer ?"]);
@@ -22,51 +23,53 @@ export default function Questionnaire(props: any) {
     }
 
     return (
-        <div className="flex justify-center">
-            <div className="md:w-2/4 lg:w-2/4 xl:w-1/4 h-80 border-2 rounded-lg shadow-md border-indigo-400">
-                <div className="flex justify-center mt-4 text-xl font-bold">
-                    {questions[questionNb]}
-                </div>
-                <div className="flex justify-center mt-12">
-                    <FormGroup>
-                        <RadioGroup
-                            aria-labelledby="demo-radio-buttons-group-label"
-                            defaultValue={options[questionNb][0]}
-                            name="radio-buttons-group"
+        <div>
+            <Header/>
+            <div className="flex justify-center mt-12">
+                <div className="md:w-2/4 lg:w-2/4 xl:w-1/4 h-80 border-2 rounded-lg shadow-md border-indigo-400">
+                    <div className="flex justify-center mt-4 text-xl font-bold">
+                        {questions[questionNb]}
+                    </div>
+                    <div className="flex justify-center mt-12">
+                        <FormGroup>
+                            <RadioGroup
+                                aria-labelledby="demo-radio-buttons-group-label"
+                                name="radio-buttons-group"
+                            >
+                                <Grid
+                                    container 
+                                    spacing={{xs: 3, md: 3, xl: 3}}
+                                    columns={{xs: 2, md: 2, xl: 2}}
+                                    className="flex justify-center"
+                                >
+                                {options[questionNb].map((option: string, index: number) => (
+                                    <Grid item key={index}>
+                                        <FormControlLabel 
+                                            value={option} 
+                                            control={<Radio/>} 
+                                            label={option}
+                                        />
+                                    </Grid>
+                                ))}
+                                </Grid>
+                            </RadioGroup>
+                        </FormGroup>
+                    </div>
+                    <div className="">
+                        <Button
+                            variant="outlined" 
+                            endIcon={<ArrowForwardIosIcon />}
+                            color="secondary"
+                            onClick={goToNextQuestion}
+                            className="top-20"
                         >
-                        <Grid
-                            container 
-                            spacing={{xs: 3, md: 3, xl: 3}}
-                            columns={{xs: 2, md: 2, xl: 2}}
-                            className="flex justify-center"
-                        >
-                        {options[questionNb].map((option: string, index: number) => (
-                            <Grid item key={index} >
-                                <FormControlLabel 
-                                    value={option} 
-                                    control={<Radio />} 
-                                    label={option}
-                                />
-                            </Grid>
-                        ))}
-                        </Grid>
-                        </RadioGroup>
-                    </FormGroup>
-                </div>
-                <div className="">
-                    <Button
-                        variant="outlined" 
-                        endIcon={<ArrowForwardIosIcon />}
-                        color="secondary"
-                        onClick={goToNextQuestion}
-                        className="top-20"
-                    >
-                        {questionNb === (questions.length - 1) ?
-                            <p>Get my Beer</p>
-                        :
-                            <p>Next question</p>
-                    }
-                    </Button>
+                            {questionNb === (questions.length - 1) ?
+                                <p>Get my Beer</p>
+                            :
+                                <p>Next question</p>
+                        }
+                        </Button>
+                    </div>
                 </div>
             </div>
         </div>
