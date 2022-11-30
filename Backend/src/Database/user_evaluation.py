14868090d7ed7c62c6220512ac45d5b38329c0a4
Antoine DESRUET <antoine.desruet@epitech.eu>
2022-11-30 17:37:08 +0900

    feat(DB): function to get user evaluation from database

--- Backend/src/Database/user_evaluation.py
+++ Backend/src/Database/user_evaluation.py
@@ -1,6 +1,8 @@
+from typing import Optional
+
 from mysql.connector import MySQLConnection
 
-from src.Database.query import execute_query
+from src.Database.query import execute_query, read_query
 from src.Scripts.init_database import connect_to_database
 
 
@@ -18,6 +20,15 @@ def increase_beer_evaluation(connection: MySQLConnection, beer_id: int) -> None:
 
 def decrease_beer_evaluation(connection: MySQLConnection, beer_id: int) -> None:
     add_evaluation_to_beer(connection, beer_id, -1)
+
+
+def get_user_evaluation(connection: MySQLConnection, beer_id: int) -> int:
+    result: Optional[list[tuple]] = read_query(connection, f'''
+        SELECT note FROM User_evaluation WHERE beer_id = {beer_id};
+    ''')
+    if not result:
+        raise ValueError(f'beer id: {beer_id}, Not found')
+    return result[0][0]
 
 
 if __name__ == "__main__":
