58f4e6afd2e7dc9144cd9b5133ada41df522da22
Guillaume Ye <guillaume.ye.pro@gmail.com>
2022-12-02 15:17:33 +0900

    Now we query all the substring from type table

--- Backend/src/Routes/recommendation/survey/POST/route.py
+++ Backend/src/Routes/recommendation/survey/POST/route.py
@@ -8,7 +8,8 @@ from src.Database.connection import create_db_connection
 
 
 def get_type(survey, connection):
-    return read_query(connection, "SELECT * FROM Beer WHERE `type` = '{}'".format(survey["type"]))
+    return read_query(connection, "SELECT * FROM Beer WHERE `type` LIKE '%{}%'".format(survey["type"]))
+    # return read_query(connection, "SELECT * FROM Beer WHERE `type` = '{}'".format(survey["type"]))
 
 def get_abv(survey, connection):
     return read_query(connection, "SELECT * FROM Beer WHERE `abv` = '{}'".format(survey["abv"]))
@@ -16,18 +17,8 @@ def get_abv(survey, connection):
 def get_ibu(survey, connection):
     return read_query(connection, "SELECT * FROM Beer WHERE `ibu` = '{}'".format(survey["ibu"]))
 
-def one_param(survey, connection):
-    db_list = None
-    if "type" in survey:
-        db_list = get_type(survey, connection)
-    elif "abv" in survey:
-        db_list = get_abv(survey, connection)
-    elif "ibu" in survey:
-        db_list = get_ibu(survey, connection)
-    elif db_list is None:
-        return "Not found"
-    else:
-        return db_list
+def get_organic(survey, connection):
+    return read_query(connection, "SELECT * FROM Beer WHERE `organic` = '{}'".format(survey["organic"]))
 
 @app.post("/recommendation")
 def recommendation_post():
@@ -36,13 +27,11 @@ def recommendation_post():
 
     connection = create_db_connection("db", 3306, "user", "password", "beer_database")
 
-    if len(survey) == 1:
-        return one_param(survey, connection)
 
     beer_list = get_type(survey, connection)
     for i in range(len(beer_list)):
         if (beer_list[i][3] >= float(survey["abvMin"]) and beer_list[i][3] <= float(survey["abvMax"])) \
-        and (beer_list[i][4] >= float(survey["ibuMin"]) and beer_list[i][4] <= float(survey["ibuMax"])):
+        and beer_list[i][9] == int(survey["organic"]):
             recommended_beer_list.append(beer_list[i])
 
     connection.close()
