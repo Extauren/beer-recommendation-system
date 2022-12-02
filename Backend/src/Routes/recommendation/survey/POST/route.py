1050d9fad68195bd96d81e3c79741571d43ef6da
Guillaume Ye <guillaume.ye.pro@gmail.com>
2022-12-02 16:52:37 +0900

    Fix beer list

--- Backend/src/Routes/recommendation/survey/POST/route.py
+++ Backend/src/Routes/recommendation/survey/POST/route.py
@@ -1,5 +1,7 @@
 from __main__ import app
 import json
+import random
+from src.RateBeer_API.BeerInfos import BeerReviewInfos
 
 from flask import request
 
@@ -8,7 +10,7 @@ from src.Database.connection import create_db_connection
 
 
 def get_type(survey, connection):
-    return read_query(connection, "SELECT * FROM Beer WHERE `type` LIKE '%{}%'".format(survey["type"]))
+    return read_query(connection, "SELECT * FROM Beer WHERE `type` LIKE '%{}%' AND `abv` >= '{}' AND `abv` <= '{}' AND `organic` = '{}'".format(survey["type"], survey["abvMin"], survey["abvMax"], survey["organic"]))
     # return read_query(connection, "SELECT * FROM Beer WHERE `type` = '{}'".format(survey["type"]))
 
 def get_abv(survey, connection):
@@ -27,12 +29,11 @@ def recommendation_post():
 
     connection = create_db_connection("db", 3306, "user", "password", "beer_database")
 
-
     beer_list = get_type(survey, connection)
-    for i in range(len(beer_list)):
-        if (beer_list[i][3] >= float(survey["abvMin"]) and beer_list[i][3] <= float(survey["abvMax"])) \
-        and beer_list[i][9] == int(survey["organic"]):
-            recommended_beer_list.append(beer_list[i])
-
+    # for i in range(len(beer_list)):
+    #     if (beer_list[i][3] >= float(survey["abvMin"]) and beer_list[i][3] <= float(survey["abvMax"])) \
+    #     and beer_list[i][9] == int(survey["organic"]):
+    #         recommended_beer_list.append(beer_list[i])
+    random_recommended_beer = random.sample(beer_list, 10)
     connection.close()
-    return recommended_beer_list, 200
+    return random_recommended_beer, 200
