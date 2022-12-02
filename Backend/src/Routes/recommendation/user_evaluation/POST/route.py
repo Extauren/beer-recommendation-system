c552ed27fa7ffaeb2af0ab1406196139b8b12905
Guillaume Ye <guillaume.ye.pro@gmail.com>
2022-12-02 14:44:25 +0900

    Add open ai user eval

--- Backend/src/Routes/recommendation/user_evaluation/POST/route.py
+++ Backend/src/Routes/recommendation/user_evaluation/POST/route.py
@@ -1,6 +1,23 @@
 from __main__ import app
 
+import os
+import openai
+
+from flask import request
+
+openai.api_key = os.getenv("OPENAI_API_KEY")
 
 @app.post("/recommendation/user_evaluation")
 def recommendation_user_evaluation_post():
-    return "Created", 201
+    user_eval = request.get_json(force=True)
+
+    response = openai.Completion.create(
+        model="text-davinci-003",
+        prompt="Decide whether a Tweet's sentiment is positive, neutral, or negative.\n\nTweet: \"{}\"\nSentiment:".format(user_eval["user_evaluation"]),
+        temperature=0,
+        max_tokens=60,
+        top_p=1.0,
+        frequency_penalty=0.5,
+        presence_penalty=0.0
+    )
+    return response, 201
